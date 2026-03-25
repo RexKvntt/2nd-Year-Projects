243ef7be747fe51560e1adcb50a25c4323f7d381
RexKvntt <drarryevr0@gmail.com>
2026-03-25 09:24:29 +0800

    Update registration form with additional fields

--- IntegrativeProgramming2/midterm/register.php
+++ IntegrativeProgramming2/midterm/register.php
@@ -10,17 +10,26 @@
 
 
     <form action="process_register.php" method="POST">
+        <label for="fullname">Full Name: </label><br>
+        <input type="text" name="fullname" required placeholder="Firstname, MI, Surname"><br><br>
+
+        <label for="phonenumber">Phone Number: </label><br>
+        <input type="text" name="phonenumber" required placeholder="(+63) 9xx xxx xxxx"><br><br>
+        
+        <label for="civilstatus">Civil Status: </label><br>
+        <input type="text" name="civilstatus" required><br><br>
+
         <label for="username">Username: </label><br>
-        <input type="text" name="username" require><br><br>
+        <input type="text" name="username" required><br><br>
 
         <label for="email">Email: </label><br>
-        <input type="email" name="email" require><br><br>
+        <input type="email" name="email" required><br><br>
 
         <label for="password">Password: </label><br>
-        <input type="password" name="password" require><br><br>
+        <input type="password" name="password" required><br><br>
 
         <label for="confirm_password">Confirm Password: </label><br>
-        <input type="password" name="confirm_password" require><br><br>
+        <input type="password" name="confirm_password" required><br><br>
 
         <button type="submit">Register</button>
 
